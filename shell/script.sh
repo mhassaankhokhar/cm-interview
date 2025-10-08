7cf3f10c71fd487e606a8499334fd14fd091795a
Mohammad <muhammadabid@192.168.1.5>
2025-10-08 16:10:48 +0500

    starting project

--- shell/script.sh
+++ shell/script.sh
@@ -1,10 +1,10 @@
 STDOUT="/dev/stdout"
-LOG_FILE='$STDOUT'
-LOG_MESSAGE='is the date, should log to $STDOUT'
+LOG_FILE="$STDOUT"
+LOG_MESSAGE="is the date, should log to $STDOUT"
 
 # log with timestamp
 log_message() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> "$LOGFILE"
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> "$LOG_FILE"
 }
 
-log_message $LOG_MESSAGE
+log_message "$LOG_MESSAGE"
